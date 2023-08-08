371b5f78938183f91a868764891531319d9e48a2
bjg60@hi.is <bjg60@hi.is>
2023-08-08 16:27:44 +0000

    update scripts and name

--- run/helper.sh
+++ run/helper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-../target/release/smly3_0 --base-path ~/blockchain --chain newrawspec.json  --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --name "`hostname`" --bootnodes=/ip4/130.208.71.224/tcp/30334/p2p/$@
+../target/release/smly3 --base-path ~/blockchain --chain newrawspec.json  --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --name "`hostname`" --bootnodes=/ip4/130.208.71.224/tcp/30334/p2p/$@
 
--- run/nuc.sh
+++ run/nuc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-../target/release/smly3_0 --base-path ~/blockchain --chain newrawspec.json --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --name "`hostname`" $@
+../target/release/smly3 --base-path ~/blockchain --chain newrawspec.json --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --name "`hostname`" $@
 
