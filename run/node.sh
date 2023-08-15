5db9b6ceff6d1d3f1e49cc771368f564cec26ecb
Bjarki Gunnarsson <bjg60@twserver.rhi.hi.is>
2023-08-15 15:05:00 +0000

    fixes

--- run/node.sh
+++ run/node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cargo run --release -- --base-path ~/blockchain --chain raw_spec.json  --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --name "`hostname`" --bootnodes=/dns/twserver.rhi.hi.is/tcp/30334/p2p/12D3KooWBKg1R3LjskMooHN548yHgD5Lpx9Ni8J9wMMbZxQS35LU
+cargo run --release -- --base-path ~/blockchain --chain raw_spec.json  --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --name "`hostname`" --bootnodes=/dns/twserver.rhi.hi.is/tcp/30333/p2p/$@
 
 
