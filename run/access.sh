9909a23567433799b200b6ad71a2a145dc427591
bjg60@hi.is <bjg60@hi.is>
2023-08-25 12:26:14 +0000

    clean up run dir

--- run/access.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cargo run --release -- --base-path ~/blockchain-access --chain raw_spec.json --rpc-port 9945 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --rpc-cors=all --name "`hostname`-access" $@
