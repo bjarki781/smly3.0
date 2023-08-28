9fb7e1b591cbee16728088ac7d7a319bee7c5802
bjarki <bjg60@hi.is>
2023-08-28 13:43:32 +0000

    fix insert key script

--- run/insert_key.sh
+++ run/insert_key.sh
@@ -3,12 +3,12 @@
 ../target/release/smly3 key insert --base-path ~/blockchain \
   --chain raw_spec.json \
   --scheme Sr25519 \
-  --suri "$1" \
+  --suri "$@" \
   --key-type babe
 
 ../target/release/smly3 key insert --base-path ~/blockchain \
   --chain raw_spec.json \
   --scheme Ed25519 \
-  --suri "$1" \
+  --suri "$@" \
   --key-type gran
 
