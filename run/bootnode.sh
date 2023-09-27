0b360657c043bade4b07604a709902ddf714fd2d
bjarki <bjg60@hi.is>
2023-09-27 16:36:00 +0000

    replace cargo run call

--- run/bootnode.sh
+++ run/bootnode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cargo run --release -- --base-path ~/blockchain --chain raw_spec.json --validator --name "bootnode-`hostname`" --unsafe-rpc-external --rpc-cors=all $@
+../target/release/smly3 --base-path ~/blockchain --chain=smly3  --validator --name "bootnode-`hostname`" --unsafe-rpc-external --rpc-cors=all $@
 
--- run/node.sh
+++ run/node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cargo run --release -- --base-path ~/blockchain --chain raw_spec.json --validator --name "`hostname`" --bootnodes=/dns/twserver.rhi.hi.is/tcp/30333/p2p/$@
+../target/release/smly3 --base-path ~/blockchain --chain=smly3 --validator --name "`hostname`" --bootnodes=/dns/twserver.rhi.hi.is/tcp/30333/p2p/$@
 
 
